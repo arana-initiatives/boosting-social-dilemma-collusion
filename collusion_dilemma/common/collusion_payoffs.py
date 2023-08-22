0326ef3d640ad375e30f570fe2115d52b6fdda90
Ashish Rana <ashishrana160796@gmail.com>
2023-08-22 14:39:16 +0200

    governance kernels with p=2 added

--- collusion_dilemma/common/collusion_payoffs.py
+++ collusion_dilemma/common/collusion_payoffs.py
@@ -16,47 +16,89 @@ def _add_payoff_noise(payoff_values, noise, const_noise=False):
         return payoff_values + round(random.uniform(-noise, noise),3)
     return payoff_values + np.round(np.random.uniform(low=-noise, high=noise, size=payoff_values.shape), 3) 
 
-def _get_pbrs_factor(payoff_values, eps_len, num_agents):
-    max_reward_arr = np.ones(num_agents, dtype=np.int8)
-    return sum(reduce(operator.getitem, max_reward_arr, payoff_values)) / eps_len
+def _get_pbrs_factor(eps_len, num_agents, hetero_prob):
+    assert hetero_prob < 1.
+    max_expct_agent_rwd = 1
+    if hetero_prob > 0:
+        max_expct_agent_rwd = 1*(1-hetero_prob) + 2*hetero_prob
+
+    return max_expct_agent_rwd / (eps_len * num_agents)
 
 def _add_temperature_tuning(payoff_values, temperature_val):
     # apply this function to payoff matrix before adding the noise
-    zero_masks, non_zero_masks = payoff_values == 0., payoff_values != 0.
-    payoff_values[zero_masks] = temperature_val
-    payoff_values[non_zero_masks] = payoff_values[non_zero_masks] - temperature_val
+    zero_masks, non_zero_masks = payoff_values < payoff_values.max() / 2, payoff_values >= payoff_values.max() / 2
+    payoff_values[zero_masks] = payoff_values[non_zero_masks] - temperature_val
+    payoff_values[non_zero_masks] = payoff_values[non_zero_masks] + temperature_val
     return payoff_values
 
 class CollusionPayoffs():
-    def __init__(self, num_agents=2, hetero_prob=0., eps_len=1, gov_rek=False):
+    def __init__(self, num_agents=2, hetero_prob=0., eps_len=1, gov_rek=False, zero_mean=False, noise=0., const_noise=False, temperature=0.):
         self.num_agents = num_agents
         self.hetero_prob = hetero_prob
-        self.agent_payoffs = self.generate_payoff_vector(self.num_agents, self.hetero_prob)
+        self.eps_len = eps_len
+        self.gov_rek = gov_rek
+        self.zero_mean = zero_mean
+        self.noise = noise
+        self.const_noise = const_noise
+        self.temperature = temperature
+        self.pbrs_factor = _get_pbrs_factor(self.eps_len, self.num_agents, self.hetero_prob)
+        self.agent_payoffs = self.generate_payoff_vector()
+        
+        
 
-    def generate_payoff_vector(self, num_agents, hetero_prof_ref):
+    def generate_payoff_vector(self):
         col_pay_vector_strat_a = np.copy(BASE_COLLUSION_PAYOFFS)
         col_pay_vector_strat_b = np.copy(BASE_COLLUSION_PAYOFFS)
-        if hetero_prof_ref > 0: # default payoff matrix starts with one heterogeneous agent
+
+        if self.gov_rek:
+            col_pay_vector_strat_a = np.copy(BASE_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(NON_ZERO_MEAN_KERNEL)
+            col_pay_vector_strat_b = np.copy(BASE_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(NON_ZERO_MEAN_KERNEL)
+            if self.zero_mean:
+                col_pay_vector_strat_a = np.copy(BASE_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(ZERO_MEAN_KERNEL)
+                col_pay_vector_strat_b = np.copy(BASE_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(ZERO_MEAN_KERNEL)
+
+        if self.hetero_prob > 0: # default payoff matrix starts with one heterogeneous agent
             col_pay_vector_strat_a = np.copy(HETERO_COLLUSION_PAYOFFS)
             col_pay_vector_strat_b = np.copy(HETERO_COLLUSION_PAYOFFS)
 
-        for kth_agent in range(num_agents-2):
+            if self.gov_rek:
+                col_pay_vector_strat_a = np.copy(HETERO_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(NON_ZERO_MEAN_KERNEL)
+                col_pay_vector_strat_b = np.copy(HETERO_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(NON_ZERO_MEAN_KERNEL)
+                if self.zero_mean:
+                    col_pay_vector_strat_a = np.copy(HETERO_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(ZERO_MEAN_KERNEL)
+                    col_pay_vector_strat_b = np.copy(HETERO_COLLUSION_PAYOFFS) + self.pbrs_factor * np.copy(ZERO_MEAN_KERNEL)
+
+        for kth_agent in range(self.num_agents-2):
             hetero_prob_ = np.random.rand()
-            if hetero_prob_ <= hetero_prof_ref:
+            if hetero_prob_ <= self.hetero_prob:
                 base_agent_payoffs = HETERO_AGENT_PAYOFFS
             else:
-                base_agent_payoffs = BASE_AGENT_PAYOFFS    
+                base_agent_payoffs = BASE_AGENT_PAYOFFS
+
+            if self.gov_rek:
+                base_agent_payoffs = {'no_collusion': base_agent_payoffs['no_collusion'],
+                                      'collusion': base_agent_payoffs['collusion'] + self.pbrs_factor * 1}
+                if self.zero_mean:
+                    base_agent_payoffs = {'no_collusion': base_agent_payoffs['no_collusion'] + self.pbrs_factor * -1 ,
+                                          'collusion': base_agent_payoffs['collusion'] + self.pbrs_factor * 1}
 
             col_pay_vector_strat_a = _update_base_payoffs(col_pay_vector_strat_a[np.newaxis, ...], base_agent_payoffs['no_collusion'])
             col_pay_vector_strat_b = _update_base_payoffs(col_pay_vector_strat_b[np.newaxis, ...], base_agent_payoffs['collusion'])
+            
             col_pay_vector_strat_a = np.append(col_pay_vector_strat_a, col_pay_vector_strat_b, axis=0)
             col_pay_vector_strat_b = np.copy(col_pay_vector_strat_a)
+
+        if self.temperature > 0:
+            col_pay_vector_strat_a = _add_temperature_tuning(col_pay_vector_strat_a, self.temperature)
+        
+        if self.noise > 0:
+            col_pay_vector_strat_a = _add_payoff_noise(col_pay_vector_strat_a, self.noise, self.const_noise)
         
         return col_pay_vector_strat_a
 
 if __name__ == "__main__":
     # homogeneous collusion payoffs
-    collusion_payoffs = CollusionPayoffs(num_agents=4)
+    collusion_payoffs = CollusionPayoffs(num_agents=4, gov_rek=True, zero_mean=True)
     print(collusion_payoffs.agent_payoffs, collusion_payoffs.agent_payoffs.shape, '\n', \
           collusion_payoffs.agent_payoffs[1][1][1][1], collusion_payoffs.agent_payoffs[1][1][1][1].shape, '\n')
     
